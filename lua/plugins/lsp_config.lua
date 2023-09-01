c0d7ba8ff192ea1c899fc90f555d10a4e41bf1b6
Elsa002 <elsa002.linux@gmail.com>
2023-09-01 15:37:47 +0300

    Fix global vim for lua

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -7,7 +7,16 @@ require("mason-lspconfig").setup_handlers {
   function (server_name) -- default handler (optional)
     require("lspconfig")[server_name].setup {}
   end,
-  ["clangd"] = function ()
-    require("lspconfig").clangd.setup {}
-  end
+  ["lua_ls"] = function ()
+    require('lspconfig').lua_ls.setup {
+      settings = {
+        Lua = {
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = {'vim'},
+          },
+        },
+      },
+    }
+  end,
 }
