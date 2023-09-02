3c34d04adb7b373664f19dbeb0e261f86b920303
Elsa002 <elsa002.linux@gmail.com>
2023-09-02 19:56:08 +0300

    Add a working custom user config

--- lua/user/get_config.lua
+++ lua/user/get_config.lua
@@ -1,8 +1,14 @@
+-- To use custom config, add a file: nvim/lua/user/user_config.lua and
+-- have the file return a table with all the settings you want to change.
+--
+-- example:
+-- return {
+--   use_dev_icons = false,
+--   smooth_scroll = false,
+-- }
 local has_user_config, user_config = pcall(require, "user/user_config")
 
-user_config = has_user_config and user_config or {}
-
-local config = {
+local default_config = {
   theme_plugin = "ful1e5/onedark.nvim",
   theme_config = function ()
     require('onedark').setup()
@@ -14,5 +20,17 @@ local config = {
   lsp_signiture = true,
   extra_plugins = {},
 }
+
+local config = has_user_config and user_config or {}
+
+local function set_default(t, key, default_value)
+  if t[key] == nil then
+    t[key] = default_value
+  end
+end
+
+for key, val in pairs(default_config) do
+  set_default(config, key, val)
+end
 
 return config
